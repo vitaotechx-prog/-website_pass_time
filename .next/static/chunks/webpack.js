067330173c80af2a481a72c957fc113316fc4064
vitaotechx-prog <vitaotechx@gmail.com>
2025-07-22 13:41:50 -0300

    fix: gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "76d8bbf08a13ae4e"; }
+/******/ 		__webpack_require__.h = function() { return "bc690498a65f1bc9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
