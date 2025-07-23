9d2bbe766ff70b8884ca566bf708ff74494fbfea
vitaotechx-prog <vitaotechx@gmail.com>
2025-07-23 02:14:29 -0300

    Fix: home.js to index.js Mudanca para convenção de nomes. Vs code Dando uma ajudinha

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a6ea3efc1cce5e4"; }
+/******/ 		__webpack_require__.h = function() { return "ce453ac157502d7d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
